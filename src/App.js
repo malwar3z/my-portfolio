b65968eae93dcbd3134efb4845075c242fbe8454
Matias Nicolas Alvarez <matias.n.alvarez@avanade.com>
2021-01-04 22:52:30 +0100

    added navigation and base components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter , Route , Switch} from 'react-router-dom';
+
+//components
+import Home from './components/Home';
+import About from './components/About';
+import Project from './components/Project';
+import Post from './components/Post';
+import SinglePost from './components/SinglePost';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Navigation/>
+        <Switch>
+          <Route component={Home} path='/' exact/>
+          <Route component={About} path='/about' />
+          <Route component={SinglePost} path='/post/:slug' />
+          <Route component={Post} path='/post' />
+          <Route component={Project} path='/project' />
+        </Switch>
+      <Footer/>
+    </BrowserRouter>
+  )
 }
 
 export default App;
